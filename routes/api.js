956bc384764a07dea204dd74aeba1c0fc55ac0d5
riddhidhawan <78215575+riddhidhawan@users.noreply.github.com>
2021-03-03 22:30:51 +0530

    Update api.js

--- routes/api.js
+++ routes/api.js
@@ -8,15 +8,15 @@ router.get('/songs', function(req, res){
     });
 });
 //add a new song to the database
-router.post('/songs', function(req, res){
+router.post('/songs', function(req, res,next){
    Music.create(req.body).then(function(song){
     res.send(song);
  
-   });
+   }).catch(next);
     
 });
 //delete a song
-router.delete('/songs/:id', function(req, res){
+router.delete('/songs/:id', function(req, res,next){
     Song.findByIdAndRemove({_id:req.params.id}).then(function(song){
         res.send(song);
     });

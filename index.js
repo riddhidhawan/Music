b25b3d427edba0f8beb2af44e9cb238694f11bc8
riddhidhawan <78215575+riddhidhawan@users.noreply.github.com>
2021-03-03 22:53:40 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const routes = require('./routes/api');
+
+
+//set up express app
+const app = express();
+
+//connect to mongodb
+mongoose.connect('mongodb+srv://riddhidhawan:<password>@cluster0.myvbj.mongodb.net/myFirstDatabase?retryWrites=true&w=majority')
+mongoose.Promise=global.Promise;
+
+
+
+app.use(bodyParser.json())
+//initializing the routes
+app.use('/api',routes);
+app.use(function(err,req,res,next){
+    res.status(422).send({error:err._message});
+});
+
+//listen for request
+app.listen(3000,function(){
+    console.log('listening for requests');
+
+});
